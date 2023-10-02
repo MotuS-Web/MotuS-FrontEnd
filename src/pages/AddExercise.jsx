75eee58f63eeb241c7a15867976bc5af22a7148c
osohyun0224 <osohyun0224@naver.com>
2023-10-02 23:00:35 +0900

    Fix: 기존 운동 등록에 대한 ProtectedRoute 연결 문제 해결

--- src/pages/AddExercise.jsx
+++ src/pages/AddExercise.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
-
+import BackButton from "../components/BackButton";
 
 const AddExercise = () => {
  
   return (
     <div>
       <Header />
+      <BackButton pageName="메인페이지" />
     </div>
   );
 };
