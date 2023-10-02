d5a3158d65b226ff737cd5da34a034e9301bda89
osohyun0224 <osohyun0224@naver.com>
2023-10-02 18:06:07 +0900

    Feat: 메인페이지 구현

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 
 const FilterSection = styled.div`
   display: flex;
@@ -11,27 +11,26 @@ const FilterSection = styled.div`
 const Title = styled.span`
   font-size: 20px;
   font-weight: bold;
-  color: #5F5F5F;
-  width: 120px; 
+  color: #5f5f5f;
+  width: 120px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   margin-right: 10px;
 `;
 
-
 const Divider = styled.div`
   width: 2px;
   height: 20px;
-  background-color: #5F5F5F;
+  background-color: #5f5f5f;
   margin-right: 10px;
 `;
 
 const Button = styled.button`
   width: 100px;
   height: 36px;
-  background-color: ${props => (props.selected ? '#6968CC' : '#1E1E1E')};
-  color: #F2F2F2;
+  background-color: ${(props) => (props.selected ? "#6968CC" : "#1E1E1E")};
+  color: #f2f2f2;
   border-radius: 10px;
   font-size: 16px;
   border: none;
@@ -43,19 +42,23 @@ const Button = styled.button`
 `;
 
 const FilterButtons = () => {
-  const [selectedCategory, setSelectedCategory] = useState('전체');
-  const [selectedPose, setSelectedPose] = useState('전체');
+  const [selectedCategory, setSelectedCategory] = useState("전체");
+  const [selectedPose, setSelectedPose] = useState("전체");
 
   return (
     <div>
       <FilterSection>
         <Title>카테고리</Title>
         <Divider />
-        {['전체', '팔', '어깨', '무릎', '허벅지'].map(category => (
+        {["전체", "팔", "어깨", "무릎", "허벅지"].map((category) => (
           <Button
             key={category}
             selected={selectedCategory === category}
-            onClick={() => setSelectedCategory(category)}
+            onClick={() => {
+              setSelectedCategory((current) =>
+                current === category ? "전체" : category,
+              );
+            }}
           >
             {category}
           </Button>
@@ -65,11 +68,13 @@ const FilterButtons = () => {
       <FilterSection>
         <Title>자세</Title>
         <Divider />
-        {['전체', '선 자세', '앉은 자세', '누운 자세'].map(pose => (
+        {["전체", "선 자세", "앉은 자세", "누운 자세"].map((pose) => (
           <Button
             key={pose}
             selected={selectedPose === pose}
-            onClick={() => setSelectedPose(pose)}
+            onClick={() => {
+              setSelectedPose((current) => (current === pose ? "전체" : pose));
+            }}
           >
             {pose}
           </Button>
