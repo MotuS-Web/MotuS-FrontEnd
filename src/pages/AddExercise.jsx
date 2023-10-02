9c143a9afaecc3bcc65d0687fcf25c5b74cfeba4
osohyun0224 <osohyun0224@naver.com>
2023-10-02 23:38:12 +0900

    Feat: 가이드 영상 등록 제목 + 설명 작성 컴포넌트 구현

--- src/pages/AddExercise.jsx
+++ src/pages/AddExercise.jsx
@@ -1,6 +1,7 @@
 import AddHeader from "../components/AddHeader";
 import VideoUploader from "../components/VideoUploader";
 import styled from "styled-components";
+import {useState} from "react";
 
 const PageContainer = styled.div`
   display: flex;
@@ -17,14 +18,81 @@ const CenteredContainer = styled.div`
   margin-top: 50px;
 `;
 
+const Title = styled.h1`
+  font-size: 24px;
+  color: #FFFFFF;
+  font-weight: bold;
+  margin-bottom: 10px;
+  margin-left:-300px;
+`;
+
+const TextContainer = styled.div`
+  margin-left: 20px;
+`;
+
+const StyledInput = styled.input`
+  width: 600px;
+  height: 50px;
+  margin-top: 10px;
+  background-color: #242424;
+  border-radius: 10px;
+  border: 1px solid #444444;
+  padding: 10px;
+  margin-bottom: 20px;
+`;
+
+const StyledTextarea = styled.textarea`
+  width: 600px;
+  height: 120px;
+  margin-top: 10px;
+  background-color: #242424;
+  border-radius: 10px;
+  border: 1px solid #444444;
+  padding: 10px;
+`;
+
+const TitleText = styled.h2`
+  font-weight: bold;
+  margin-bottom: 10px;
+  font-size: 24px;
+  color: #FFFFFF;
+  margin-bottom: 10px;
+`;
 
 const AddExercise = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDesctiption] = useState("");
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDesctiption(e.target.value);
+  };
   return (
     <div>
       <PageContainer>
       <AddHeader />
       <CenteredContainer>
+      <Title>동영상 및 스켈레톤 데이터</Title>
       <VideoUploader/>
+      <TextContainer>
+          <TitleText>운동 제목</TitleText>
+          <StyledInput
+            placeholder="제목을 입력하세요..."
+            maxLength={50}
+            value={title}
+            onChange={handleTitleChange}
+          />
+          <TitleText>운동 설명</TitleText>
+          <StyledTextarea
+            placeholder="설명을 입력하세요..."
+            maxLength={200}
+            value={description}
+            onChange={handleDescriptionChange}
+          />
+        </TextContainer>
       </CenteredContainer>
       </PageContainer>
     </div>
