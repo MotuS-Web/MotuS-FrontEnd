94a268db10549245a065a783ec4879c7a2c94beb
osohyun0224 <osohyun0224@naver.com>
2023-10-05 22:26:02 +0900

    Fix: tags와 백엔드 태그 설정 불일치로 인해 뜨지 않는 매핑 오류 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -22,7 +22,6 @@ const ExerciseContainer = styled.div`
   gap: 48px 60px;
 `;
 
-
 const MainPage = () => {
   const [list, setList] = useState([]);
   const [course, setCourse] = useState(null);
@@ -30,18 +29,17 @@ const MainPage = () => {
   useEffect(() => {
     async function fetchCourses() {
       try {
-        const response = await getPrograms(); 
+        const response = await getPrograms();
+        console.log("API Response:", response); // 여기를 추가
         if (response && response.dtoList) {
           setList(response.dtoList);
         }
       } catch (error) {
-        console.error('운동 목록을 불러오지 못했습니다.', error);
+        console.error("운동 목록을 불러오지 못했습니다.", error);
       }
     }
-  
     fetchCourses();
   }, []);
-  
 
   function openModal(id) {
     setCourse(list.filter((item) => item.id === id)[0]);
@@ -57,13 +55,17 @@ const MainPage = () => {
           onClose={() => setCourse(null)}
           {...course}
         />
-        {list.map((course) => (
-          <ExerciseCard
-            key={course.id}
-            onClick={() => openModal(course.id)}
-            {...course}
-          />
-        ))}
+        {list.map((course) => {
+          const tags = [course.category, course.position];
+          return (
+            <ExerciseCard
+              key={course.vno}
+              onClick={() => openModal(course.vno)}
+              tags={tags}
+              {...course}
+            />
+          );
+        })}
       </ExerciseContainer>
     </PageContainer>
   );
